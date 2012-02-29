a7b78496c87e2c0cf4b0eb1408aca64bbf544af0
Jim Svensson <jimpa@tla.se>
2012-02-29 21:25:19 +0100

    Added JavaDoc.

--- src/test/java/se/tla/mavenversionbumper/ModuleTinker.java
+++ src/test/java/se/tla/mavenversionbumper/ModuleTinker.java
@@ -1,12 +1,12 @@
 package se.tla.mavenversionbumper;
 
 /**
- * Created by IntelliJ IDEA.
- * User: jimpa
- * Date: 2/26/12
- * Time: 1:34 AM
- * To change this template use File | Settings | File Templates.
+ * Helper interface for the ModuleTestTemplate class.
  */
 interface ModuleTinker {
+    /**
+     * Perform tinkering on the subject.
+     * @param subject
+     */
     void tink(Module subject);
 }
